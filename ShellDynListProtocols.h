a39c455d9f089c289b50528d41528e513ce64cdb
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2024-05-30 16:37:56 +0000

    Update ShellDynListProtocols.h

--- ShellDynListProtocols.h
+++ ShellDynListProtocols.h
@@ -54,5 +54,7 @@ EFIDynCmdProtocolLpGetHelp(
     IN EFI_SHELL_DYNAMIC_COMMAND_PROTOCOL  *This,
     IN CONST CHAR8                         *Language
     );
+extern EFI_BOOT_SERVICES *gBS;
+extern EFI_SYSTEM_TABLE *gST;
 
 #endif /* __SHELL_DYN_LIST_PROTOCOLS_H__ */
