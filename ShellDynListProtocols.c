d9a9c15016c71d8019d6eb9ee463d483a430a185
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-02 17:21:56 +0000

    Update ShellDynListProtocols.c

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -183,7 +183,7 @@ EFIDynListProtocolsEntryPoint(
             FreePool(ArgValue);
             ShellCommandLineGetRawValue(Package, Index);
             Print(L"Argument %d: %08X\n", Index, ArgValue); // Print or process the argument value
-            Print(L"Argument %d: %s\n", Index, &ArgValue); // Print or process the argument value
+            Print(L"Argument %d: %s\n", Index, *ArgValue); // Print or process the argument value
             if (!EFI_ERROR(IsHexadecimal(ArgValue)))
             {
                 EFIDynCmdProtocolLpHandlerbyhandle((EFI_HANDLE *)ArgValue);
