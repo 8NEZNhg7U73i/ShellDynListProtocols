bcc6c29da9bfc42a94a6e5b37cd517e933a9b5cc
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-03 16:47:31 +0000

    mb

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -188,7 +188,7 @@ EFIDynListProtocolsEntryPoint (
     EFI_SHELL_PARAMETERS_PROTOCOL *ShellParameters;
     UINTN ParamCount = 0;
     CHAR16 *ParamStr = NULL;
-    UINTN64 *ParamInt = NULL;
+    UINT64 *ParamInt = NULL;
 
     Status = ShellInitialize();
 
