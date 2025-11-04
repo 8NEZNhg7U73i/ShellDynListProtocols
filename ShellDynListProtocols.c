b6a8bc59b7e772a1c00bbc83af8c02ed5c5e0fba
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-04 05:41:04 +0000

    fe

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -229,9 +229,9 @@ EFIDynListProtocolsEntryPoint (
             Status = ShellConvertStringToUint64(ParamStr, ParamInt, FALSE, TRUE);
             if (!EFI_ERROR(Status))
             {
-                Print(L"arg %d is vaild hex text, %08X, Pointer location: %p\n", i, *ParamInt, ParamInt);
+                Print(L"arg %d is vaild hex text, %08X, Pointer location: %p, GUID: %g\n", i, *ParamInt, ParamInt, gEfiDevicePathProtocolGuid);
                 Status = EFIDynCmdProtocolLpHandler((EFI_HANDLE*)ParamInt, &gEfiDevicePathProtocolGuid);
-                Print(L"arg %d is vaild hex text, %08X, Pointer location: %p\n", i, *ParamInt, ParamInt);
+                Print(L"arg %d is vaild hex text, %08X, Pointer location: %p, GUID: %g\n", i, *ParamInt, ParamInt, gEfiDevicePathProtocolGuid);
             }
             else 
             {
