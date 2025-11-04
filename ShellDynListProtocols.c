970c41518ffa9d047816252956a1c9206f788a3a
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-04 16:05:56 +0000

    fe

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -120,8 +120,8 @@ EFIDynCmdProtocolLpHandler(IN EFI_HANDLE *InputHandle OPTIONAL, IN EFI_GUID *Inp
         for (ProtocolIndex = 0; ProtocolIndex < ProtocolCount; ProtocolIndex++)
         {
             Print(L"ProtocolIndex: %d\n", ProtocolIndex);
+            Print(L"%X, %X, %X, %X\n",HandleBuffer[HandleIndex], ProtocolBuffer[ProtocolIndex], &OpenInfo, &OpenInfoCount);
             Status = gBS->OpenProtocolInformation(HandleBuffer[HandleIndex], ProtocolBuffer[ProtocolIndex], &OpenInfo, &OpenInfoCount);
-            Print(L"%X, %X, %X, %X",HandleBuffer[HandleIndex], ProtocolBuffer[ProtocolIndex], &OpenInfo, &OpenInfoCount);
             if (!EFI_ERROR(Status))
             {
                 if (0 == ProtocolIndex)
