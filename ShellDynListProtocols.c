b257f1f9f5e83c450cd9d5217e8cb7a774aadc0d
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-06 12:31:06 +0000

    frr

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -43,7 +43,7 @@ STATIC EFI_HANDLE mEFIDynListProtocolsHiiHandle;
 
 EFI_STATUS
 EFIAPI
-EFIDynCmdProtocolLpHandler(IN EFI_HANDLE *InputHandle OPTIONAL, IN EFI_GUID *InputGuid OPTIONAL)
+EFIDynCmdProtocolLpHandler(IN EFI_HANDLE *InputHandle OPTIONAL, IN EFI_GUID *InputGuid OPTIONAL, IN UINTN InputHandleCount, IN UINTN InputProtocolCount)
 {
     //EFI_SYSTEM_TABLE  *SystemTable;
     //EFI_BOOT_SERVICES *gBS = SystemTable->BootServices;
