123218b56a042fce21b7113f0bb848449f4d2d45
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-06 15:39:25 +0000

    fe

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -19,7 +19,6 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 
-#include <Protocol/LoadedImage.h>
 #include <Protocol/ShellParameters.h>
 
 #include <Library/BaseMemoryLib.h>
