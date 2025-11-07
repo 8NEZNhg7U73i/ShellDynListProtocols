1dfbbcf76349c28e94f1d9f4a20dde63c70cdbb2
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-07 04:53:17 +0000

    ef

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -221,7 +221,7 @@ EFIDynListProtocolsEntryPoint (
             if (!EFI_ERROR(Status))
             {
                 Print(L"arg %d is vaild hex text, %08X, Pointer location: %p, GUID: %g\n", i, *ParamInt, ParamInt, gEfiPartitionInfoProtocolGuid);
-                VOID test[10] = {gEfiDevicePathProtocolGuid};
+                EFI_GUID **test[10] = {gEfiDevicePathProtocolGuid};
                 Status = EFIDynCmdProtocolLpHandler((EFI_HANDLE *)ParamInt, (EFI_GUID **)&test, 1, 6);
             }
             else 
