fb23e4cb28202a284ee6440431cd6e1df54f430e
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-07 15:46:07 +0000

    Update ShellDynListProtocols.c

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -20,7 +20,7 @@
 
 EFI_STATUS
 EFIAPI
-EFIDynCmdProtocolLpHandler()
+EFIDynCmdProtocolLpHandler(IN EFI_HANDLE *InputHandleBuffer OPTIONAL, IN EFI_GUID **InputProtocolBuffer OPTIONAL, IN UINTN InputHandleCount, IN UINTN InputProtocolCount)
 {
     EFI_STATUS Status;
     UINTN HandleCount;
