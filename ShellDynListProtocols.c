1ea86adaa15d788d5d0c068f9417d9fa7805da1b
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-07 03:03:03 +0000

    fe

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -77,7 +77,7 @@ EFIDynCmdProtocolLpHandler(IN EFI_HANDLE *InputHandleBuffer OPTIONAL, IN EFI_GUI
                 return EFI_OUT_OF_RESOURCES;
             }
             Print(L"&InputProtocolBuffer: %p, InputProtocolBuffer: %g\n", &InputProtocolBuffer, InputProtocolBuffer);
-            CopyMem(ProtocolBuffer, InputProtocolBuffer, sizeof(EFI_GUID) * InputProtocolCount);
+            CopyMem(*ProtocolBuffer, *InputProtocolBuffer, sizeof(EFI_GUID) * InputProtocolCount);
             Print(L"EFI_GUID: %p, value:%g\n", ProtocolBuffer, ProtocolBuffer);
             ProtocolCount = InputProtocolCount;
         }
