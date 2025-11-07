f170b7ce84468403b51bf6c3e87e02a2e2f7851c
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-07 17:09:07 +0000

    fe

---
+++ ShellDynListProtocols.h
@@ -0,0 +1,3 @@
+EFI_STATUS
+EFIAPI
+EFIDynCmdProtocolLpHandler(IN EFI_HANDLE *InputHandleBuffer OPTIONAL, IN EFI_GUID **InputProtocolBuffer OPTIONAL, IN UINTN InputHandleCount, IN UINTN InputProtocolCount);
