f049eb091e46a2f09cbebf481a084e5f76236c60
8NEZNhg7U73i <68855943+8NEZNhg7U73i@users.noreply.github.com>
2025-11-08 02:58:42 +0000

    fe

--- ShellDynListProtocols.c
+++ ShellDynListProtocols.c
@@ -107,8 +107,8 @@ EFIDynCmdProtocolLpHandler(IN EFI_HANDLE *InputHandleBuffer OPTIONAL, IN EFI_GUI
                     {
                         Print(L"DevicePath is NULL\n");
                     }
-                    Print(L"|   Handle   |    |           Protocol GUID           |               | Agent |     | Controller |     | Attributes|         | OpenCount |  \n");
-                    Print(L" 0X%08X: %g\n", HandleBuffer[HandleIndex], ProtocolBuffer[ProtocolIndex]);
+                    Print(L"|   Handle   |  |           Protocol GUID           |               | Agent |     | Controller |     | Attributes|         | OpenCount |  \n");
+                    Print(L"  0X%08X: %g\n", HandleBuffer[HandleIndex], ProtocolBuffer[ProtocolIndex]);
                 }
                 else
                 {
